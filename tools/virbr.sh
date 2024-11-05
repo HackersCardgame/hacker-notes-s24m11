8a7eddd5765f1d92d060e2ca36428824984b2c0e
Your Name <you@example.com>
2024-11-05 04:34:55 +0100

    ...

--- tools/virbr.sh
+++ tools/virbr.sh
@@ -95,81 +95,10 @@ echo systemctl restart NetworkManager
 
 read
 
-apt-get update -y
-apt-get upgrade -y
-apt-get install -y screen
-apt-get install -y gdm3
-apt-get install -y gnome
-apt-get install -y mpg123
-apt-get install -y alsa-utils
-apt-get install -y python3-mido python3-rtmidi python3-pulsectl
-apt-get install -y gnome-shell
-apt-get install -y gnome-backgrounds
-apt-get install -y wmctrl
-apt-get install -y xdotool
-
-apt-get purge -y cups*
-apt-get purge libcupsfilters2*
-apt-get install -y cups
-apt-get install cups-client -y
-apt-get install -y gedit
-apt-get install -y gnome-terminal
-apt-get install -y gimp
-apt-get install -y calibre
-apt-get purge firefox-esr
-apt-get install -y epiphany-browser
 #apt-get install -y git vim sudo gnome-boxes figlet inkscape gedit mixxx 
 apt-get install -y qemu-system qemu-kvm libvirt-daemon bridge-utils virt-manager 
-
 echo apt cpu-checker
 
-apt-get install -y git vim sudo figlet inkscape gedit mixxx 
-apt-get install -y libvirt-daemon-system 
-apt-get install -y ffmpeg
-apt-get install -y kdenlive
-apt-get install -y obs-studio
-apt-get install -y tesseract-ocr
-apt-get install -y nvidia-detect
-apt-get install -y locate
-
-fgrep -ri PRUNEPATHS /etc/ 2>/dev/null
-
-if YESNO "edit PRUNEPATHS and updatedb ???"
-then
-#echo run /old2/home/marc/hacker-notes-s??m??/tools/mount.sh
-echo mount all drives with eg. nautilus in gui with that user you normally use
-#su marc
-
-echo pressanykey
-read
-find /media/ -type d -maxdepth 2
-read
-vim /etc/cron.daily/locate -c "/PRUNEPATH"
-updatedb
-fi
-apt-get install -y apt-file
-apt-file update
-apt-get install -y espeak-ng
-apt-get install -y pavumeter
-apt-get install -y callibre
-apt-get install -y iotop
-apt-get install -y xxd
-
-apt-get install -y libreoffice
-apt-get install -y nautilus
-
-apt-get install -y eog
-
-apt-get install -y pavucontrol
-apt-get install -y rdfind
-apt-get install -y rsync
-apt-get install -y asunder
-apt-get install -y brasero
-apt-get install -y mdadm
-apt-get install -y smarttools
-apt-get install -y jigdo-file
-apt-get install -y wodim
-wodim --devices
 
 echo sudo wodim -v -data dev=/dev/sr0  ...iso
 echo sha512 ...iso
@@ -180,64 +109,6 @@ echo "dd if=/dev/sr0 bs=2048 | sha512"
 
 
 
-apt-get install -y xdotool wmctrl
-
-
-apt-get install cups-client -y
-apt-get install tesseract-ocr-eng tesseract-ocr-deu -y
-
-
-if YESNO "wine, wine64 und wine32:i386 installieren ???"
-then
-
-apt-get install -y wine
-apt-get install -y wine64
-apt-get install -y winetricks
-
-echo dann \"winetricks d3dx9\" und \"winetricks vcrun2010\" eingeben mit dem Benutzer
-
-dpkg --add-architecture i386
-apt-get update
-apt-get install wine32:i386
-
-fi
-
-apt-get install -y xdotool wmctrl
-
-
-apt-get install cups-client -y
-apt-get install tesseract-ocr-eng tesseract-ocr-deu -y
-
-echo "printer installieren"
-echo ""
-echo "printer installieren"
-echo ""
-echo "printer installieren"
-echo ""
-echo "printer installieren"
-echo ""
-echo "printer installieren"
-
-nvidia-detect
-
-# edit repository list
-#==============================================================================
-if YESNO "install nvidia-tesla-470-driver ???"
-then
-apt-get install nvidia-tesla-470-driver
-fi
-
-echo apt kvm-ok
-
-
-# edit repository list
-#==============================================================================
-if YESNO "libvirtd starten und installieren?"
-then
-  systemctl start libvirtd
-#  systemctl enable libvirtd
-#  dann virt-manager starten
-fi
 
 if YESNO "add bridge"
 then
@@ -254,32 +125,10 @@ if YESNO "user zur libvirt gruppe hunzufügen?"
 then
   usermod -aG libvirt marc
 fi
-
-apt-get install jigdo-file
-
-#apt-get install mbrola mbrola-de* mbrola-us* mbrola-en1
-apt-get install espeak espeak-ng
-
-cp /usr/share/xsessions/*xorg* /usr/share/xsessions/TEST.desktop
-apt-get purge libcupsfilters2*
-
-
 red="\e[91m"
 red2="\e[95m"
 default="\e[39m"
 
-echo -e "${red}printer installieren"
-echo ""
-echo -e "${red2}printer installieren"
-echo ""
-echo -e "printer installieren"
-echo ""
-echo -e "${red}printer installieren"
-echo ""
-echo -e "${red2}printer installieren"
-echo ""
-echo -e "printer installieren${default}"
-echo ""
 
 echo brctrl show
 brctl addbr virbr1
@@ -287,3 +136,4 @@ ip link set virbr1 up
 
 journalctl -xe |grep network -i
 
+brctl show && brctl addbr virbr1 && brctl show && ip link set virbr1 up && brctl show && ip addr
