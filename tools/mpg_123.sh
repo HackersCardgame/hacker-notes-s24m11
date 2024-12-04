58bf8e45fea46ed9213dfced4cc10816d675513a
Your Name <you@example.com>
2024-12-04 20:33:49 +0100

    ...

--- tools/mpg_123.sh
+++ tools/mpg_123.sh
@@ -2,4 +2,4 @@
 
 find /media/marc/77*/Music.merged/ -name "*3D0c*" -type f -print0 | shuf -z | xargs -0  mpg123  
 
-find /media/marc/771a9f36-8da1-4138-97c2-ec12ed9ef476/Music.merged/Faun/ -name "*a*" -type f -print0 | shuf -z | xargs -0  mpg123  
+#find /media/marc/771a9f36-8da1-4138-97c2-ec12ed9ef476/Music.merged/Faun/ -name "*a*" -type f -print0 | shuf -z | xargs -0  mpg123  
