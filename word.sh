cf0d9111e7810e92edf88bc9de2d637753efbd6b
Your Name <you@example.com>
2024-12-10 08:10:09 +0100

    ...

---
+++ word.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+
+for i in $(echo OPdeatheaters PizzaGate QAnon Adrenochrom Epstein und all diese anderen Dinge verschleiern perfekt DASS Sex-Sekten dazu da sind WirtschaftsSpionage zu erzwingen DASS Sex-Sekten dazu da sind WirtschaftsSpionage zu erzwingen DASS Sex-Sekten dazu da sind WirtschaftsSpionage zu erzwingen)
+do
+	echo $i
+	figlet $i -w 200
+	sleep 1
+	reset
+done
---
+++ word2.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+
+for i in $(echo die KeyWord Kombination oder Selektor-KOMBINATION Sex-Sekte UND WirtschaftsSpionage aktivert scheinbar eines der groessten CyberTERROR Netzwerke gegen mich)
+do
+	echo $i
+	figlet $i -w 200
+	sleep 1
+	reset
+done
