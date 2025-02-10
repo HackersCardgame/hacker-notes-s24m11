bdc2f6d295be1b1b3380e18f31e0184a8cc13024
Your Name <you@example.com>
2025-02-10 07:50:05 +0100

    .....

--- tools/qemu.resize.sh
+++ tools/qemu.resize.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 # qemu-img resize ......qcow2 50G
+
 # zB qemu-img resize /var/lib/libvirt/images/xyz.qcow2 30G
+also die zahl gibt an was danach total verwendet werden soll, also das ist nicht 30GB mehr sondern > total 30GB <
 
 #
 # in der VM: growpart /dev/vda 1
