88b75749b436887fea4313352ede55e0395ae6f6
Your Name <you@example.com>
2025-02-16 02:58:12 +0100

    .....

--- tools/combined.soundcard.sh
+++ tools/combined.soundcard.sh
@@ -7,3 +7,23 @@ pactl info |grep "Server Name" --color
 pactl list sinks
 pactl list sinks |grep -E 'Name:|State:' --color
 
+
+# pw-link alsa_output.pci-0000_00_1f.3.analog-stereo.2 alsa_output.pci-0000_00_1f.3.iec958-stereo 
+# ODER
+# pactl load-module module-combine-sink sink_name=combinedSoundCard slaves=output.pci-0000_00_1f.3.analog-stereo.2,alsa_output.pci-0000_00_1f.3.iec958-stereo 
+
+pactl list modules |grep module-combine-sink -B 2 -A 20 --color
+
+# Beispiel
+#Module #536870916
+#	Name: module-combine-sink
+#
+#
+# pactl unload-module 536870916
+# pactl unload-module module-combined-sink
+#
+# pw-link output.pci-0000_00_1f.3.analog-stereo.2 alsa_output.pci-0000_00_1f.3.iec958-stereo
+#
+# journalctr -u pipewire -f
+#
+# systemctl --user restart pipewire
